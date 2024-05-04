db6a6acaafeeec8d6055e6c3b69a82d0145609c0
Karam.Sidhu <Karam02sidhu@gmail.com>
2024-05-04 10:20:05 -0600

    5 passed, gave up 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const remover_help = function(array, choice) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] == choice) {
+            array.splice(i,2);
+        }
+    }
+    return array
+}
 
+const removeFromArray = function(array, a, b) {
+    for (let i = 1; i < arguments.length; i++) {
+        if (arguments[i] != undefined)
+            remover_help(array, arguments[i]);
+    }
+
+    return array
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(one, two) {
+    let sum = 0;
+    
+    
+    if (one < 0 || two < 0) {
+        return "ERROR"
 
+    } else if (Number.isInteger(one) & Number.isInteger(two)) {
+
+        if (one > two) {
+            let temp = one;
+            one = two;
+            two = temp;
+        }
+
+        for (let i = one; i <= two; i++) {
+           sum += i;
+        }
+        return sum;
+    
+    } else {
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
