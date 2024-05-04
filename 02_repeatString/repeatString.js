379beb688af817f678ea2d4091d7e7e1dc9525e9
Karam.Sidhu <Karam02sidhu@gmail.com>
2024-05-04 08:21:38 -0600

    2 passed all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, numb) {
 
+    if (numb < 0) {
+        return "ERROR"
+    }
+
+
+    let output = ""
+    for (let index = 0; index < numb; index++) {
+        output = output.concat(string);
+    }
+    return output
 };
 
 // Do not edit below this line
