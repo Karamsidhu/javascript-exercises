b7c529bac6c6f016b33e94e3dc41bfb4d32610a3
Karam.Sidhu <Karam02sidhu@gmail.com>
2024-05-04 08:31:57 -0600

    3 passed tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let word = "";
 
-};
+    for (let i = 0; i < string.length; i++) {
+        word += string[(string.length-1)-i];
+    }
+    return word
+}
 
 // Do not edit below this line
 module.exports = reverseString;
